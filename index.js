bcf8052bf21e90a331b45086fae3f3da616686cc
Dave Banwell <dave@davebanwell.com>
2024-05-11 02:03:59 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1');
+document.body.insertBefore(newHeader, document.body.firstChild);
+
+newHeader.id = 'victory';
+
+newHeader.textContent = 'Dave is the champion';
